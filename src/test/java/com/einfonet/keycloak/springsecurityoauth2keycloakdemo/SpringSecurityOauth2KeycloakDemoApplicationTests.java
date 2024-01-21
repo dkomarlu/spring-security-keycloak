b726fb399cb372d04f6a3c380429910e920241cc
Dharani <dkomarlu@yahoo.com>
2024-01-20 22:07:30 -0500

    Initial

---
+++ src/test/java/com/einfonet/keycloak/springsecurityoauth2keycloakdemo/SpringSecurityOauth2KeycloakDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.einfonet.keycloak.springsecurityoauth2keycloakdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2KeycloakDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
